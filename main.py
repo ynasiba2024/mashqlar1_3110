9416bc5f562fed08411776b0e296caf85a0b4f4b
ynasiba2024 <t7979176@gmail.com>
2025-10-31 18:04:12 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1-misol
+for i in range (1,10):
+    print(i)
+  
+
+
+# 2-misol
+for i in range (10,1):
+    print(i)
+
+
+# 3-misol
+n = int(input("Son kirit: "))
+for i in range(0, n , 2):
+    print(i)
+
+#4-misol
+n = int(input("Son kirit: "))
+for i in range(1,n,2):
+    print(i)
